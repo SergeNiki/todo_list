eb7f46775f13d2683cc763e5318487957401d743
SergeNiki <sergey.nikitenko.2017@mail.ru>
2022-09-29 21:22:29 +0500

    fix gitignor

--- client/src/http/usersAPI.js
+++
@@ -1,9 +0,0 @@
-import $api from "./index";
-
-const usersAPI = {
-    getUsers: async (usersRole) => {
-        return $api.get(`/users?role=${usersRole}`)
-    }
-}
-
-export default usersAPI
--- server/routes/users-router.js
+++
@@ -1,8 +0,0 @@
-const Router = require('express');
-const router = new Router();
-const usersController = require('./../controllers/users-controller');
-const authenticateToken = require('./../middleware/authMiddleware');
-
-router.get('/users', authenticateToken, usersController.getUsers);
-
-module.exports = router;
